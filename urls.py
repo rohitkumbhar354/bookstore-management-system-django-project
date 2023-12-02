0039a88c3bf08f9c145a13071892a0679847edf8
rohitkumbhar354 <141200734+rohitkumbhar354@users.noreply.github.com>
2023-12-02 09:52:59 +0530

    Add files via upload

--- urls.py
+++ urls.py
@@ -1,25 +1,10 @@
-from django.urls import path
-from . import views
+
+from django.contrib import admin
+from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
-    path("", views.index, name="index"),
-    path("signup/", views.Usersignup, name="Usersignup"),
-    path("user_login/", views.User_login, name="User_login"),
-    path("logout/", views.Logout, name="Logout"),
-    
-#     For admin
-    path("all_books/", views.Admin, name="Admin"),
-    path("customers_list/", views.customers_list, name="customers_list"),
-    path("add_books/", views.Add_Books, name="add_new_books"),
-    path("all_books/delete_<int:myid>/", views.Delete_Books, name="delete_book"),
-    path("see_requested_books/", views.see_requested_books, name="see_requested_books"),
-    path("delete_requested_books/delete_<int:myid>/", views.delete_requested_books, name="delete_requested_books"),
-    path("customers_list/orders_<int:myid>/", views.orders_list, name="orders_list"),
-    path("customers_list/orders_<int:myid>/data/", views.data_view, name="data"),
- 
-    
-#     For customers
-    path("for_users/", views.Users, name="Users"),
-    path("request_books/", views.request_books, name="request_books"),   
-    path("checkout/", views.checkout, name="checkout"),    
-]
+    path('admin/', admin.site.urls),
+    path('', include('books.urls')),
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
